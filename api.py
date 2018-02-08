a9343696a8e61498c767d5a2d697e49083e8e147
Chimdindu Orakwue <denalexo@cs.utexas.edu>
2018-02-08 17:40:07 -0600

    updated Flask files

--- api.py
+++ api.py
@@ -1,3 +1,7 @@
+"""
+endpoints for api and website
+"""
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -9,9 +13,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def hello_world():
 	return "Hello World!"
 
-@app.route('/<test-name>')
-def hello_world():
+@app.route('/<name>')
+def hello_world_name():
 	return "Hello {}!".format(name)
 
+@app.route('/city')
+def city_page():
+	return "Hello World!"
+
+@app.route('/photos')
+def photo_page():
+	return "Hello World!"
+
+@app.route('/park')
+def park_page():
+	return "Hello World!"
+
+@app.route('/park/<id>')
+def get_park_by_id():
+	return "Hello World!"
+
+@app.route('/city/<id>')
+def get_city_by_id():
+	return "Hello World!"
+
+@app.route('/photos/<id>')
+def get_photo_by_id():
+	return "Hello World!"
+
 if __name__ == '__main__':
-	app.run()
+	app.run(port = 5000, use_reloader=True, threaded=True)
