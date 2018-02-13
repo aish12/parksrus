0beda28e65df6d78dcd7aa5cd5d03d697372831a
Chimdindu Orakwue <denalexorakwue1@gmail.com>
2018-02-12 20:47:18 -0600

    added comments to Flask code

--- app/app.py
+++ app/app.py
@@ -6,20 +6,25 @@ from flask_restless import APIManager
 from config import REACT_FILES, BASE_DIR
 import os
 
+#API manager to create API's with flask_restless
 manager = APIManager(app, flask_sqlalchemy_db=db)
 model_objects = [Park, Photo, City]
 
+# create tables for each model
 db.create_all()
 
+# arguments for API (methods allowed, pagination, etc)
 kwargs = {
 	'methods': frozenset(['GET', 'POST', 'PATCH']),
     'allow_functions': True,
     'results_per_page': 8
 }
 
+# create API endpoint for each model
 for model in model_objects:
 	manager.create_api(model, **kwargs)
 
+#serve the React app
 @app.route('/', defaults={'path':''})
 @app.route('/<path:path>')
 def serve_react(path):
@@ -31,5 +36,6 @@ def serve_react(path):
 	else:
 		return send_from_directory(REACT_FILES, 'index.html')
 
+#run on port 5000
 if __name__ == '__main__':
 	app.run(port = 5000, use_reloader=True, threaded=True)
--- app/config.py
+++ app/config.py
@@ -2,10 +2,15 @@
 configuration file
 """
 import os
+
+# BASE_DIR is directory above config.py
 BASE_DIR = os.path.abspath(os.path.dirname('__file__'))
+
+# append path BASE_DIR to parksrus-frontend/build to get absolute path
 REACT_FILES = os.path.join(BASE_DIR, 'parksrus-frontend/build')
 
 class Config(object):
+	# get database url from environmental variable
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	DEBUG = False
