4791ee4d661ec48d9fcce167b2e200c679dbd3ef
Chimdindu Orakwue <denalexorakwue1@gmail.com>
2018-02-12 20:40:08 -0600

    scraper.py preliminary interface

--- app/scraper.py
+++ app/scraper.py
@@ -1,3 +1,19 @@
 """
 Script that will scrape API's to gather info for database
 """
+
+from googleplaces import GooglePlaces, types, lang
+
+API_KEY = os.environ['GOOGLE_PLACES_KEY']
+
+def city_wide_search(latitude, longitude):
+
+def search_parks_in_city(latitude, longitude):
+
+def search_parks_in_city(city, country):
+
+def get_coordinates(city):
+
+def get_coordinates(city, country):
+
+def get_coordinates_park(park):
