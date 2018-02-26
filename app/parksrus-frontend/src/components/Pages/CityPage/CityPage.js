01b38b6bf02de9db976a1a291000937fdcf56e4b
Trenton Beckendorff <trentonbeckendorff@utexas.edu>
2018-02-25 20:19:41 -0600

    Temporarily removing city pages' parks card

--- app/parksrus-frontend/src/components/Pages/CityPage/CityPage.js
+++ app/parksrus-frontend/src/components/Pages/CityPage/CityPage.js
@@ -73,13 +73,13 @@ class CityPage extends React.Component {
                           imageHeight={'450px'}/>
               </PageSection>
               <PageSection header={"Nearby Park Attractions"}>
-                <Card classes={"CityCard"} key={city.parks[0].id}>
-                  <Link to={'/parks/' + city.parks[0].id} className={"CardLink"}>
-                    <img src={city.parks[0].image_uri} className="CardImage" style={{height: "500px"}}/>
-                    <h1 className={["CardContent", "CardHeader"].join(' ')}>{city.parks[0].name}</h1>
-                    <p className={["CardContent", "CardSubtitle"].join(' ')}>{city.parks[0].state}</p>
-                  </Link>
-                </Card>
+                {/*<Card classes={"CityCard"} key={city.parks[0].id}>*/}
+                  {/*<Link to={'/parks/' + city.parks[0].id} className={"CardLink"}>*/}
+                    {/*<img src={city.parks[0].image_uri} className="CardImage" style={{height: "500px"}}/>*/}
+                    {/*<h1 className={["CardContent", "CardHeader"].join(' ')}>{city.parks[0].name}</h1>*/}
+                    {/*<p className={["CardContent", "CardSubtitle"].join(' ')}>{city.parks[0].state}</p>*/}
+                  {/*</Link>*/}
+                {/*</Card>*/}
               </PageSection>
             </Page>
           </div>
