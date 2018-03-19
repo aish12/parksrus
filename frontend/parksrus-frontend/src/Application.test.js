858ce81926ddfd94293d67ca92e2f10ba62af75e
Daniel Zheng <daniel.c.zheng@gmail.com>
2018-03-19 17:27:35 -0500

    Revert "Setup for Mocha. No unit tests for React yet."
    This reverts commit 2a952e2524bd05210de1afd73d74f659eb876188.

--- frontend/parksrus-frontend/src/Application.test.js
+++ frontend/parksrus-frontend/src/Application.test.js
@@ -1,22 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './Application';
-//
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Application';
 
-import React from 'react'
-import expect from 'expect'
-import { shallow, mount } from 'enzyme'
-import App from './Application'
-
-const wrapper = shallow(<App />);
-describe('App Component', () => {
-    // TODO: this doesn't do anything yet
-    it('title renders without crashing', () => {
-        expect(wrapper.find('title').text()).toEqual('parksr.us')
-    })
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
