2a8b5990e5ebdbdfeb17a623c15baa17b15f33f8
Ben Chen <btchen@utexas.edu>
2018-03-26 20:09:40 -0500

    fixed gui tests

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -30,14 +30,14 @@ class SeleniumTests(unittest.TestCase):
         driver.get(self.site)
 
         driver.find_element_by_link_text('Parks').click()
-        self.assertEqual("http://parksr.us/#/parks", driver.current_url)
+        self.assertEqual("http://parksr.us/#/parks/pages/1", driver.current_url)
 
         time.sleep(1)
 
         park_instance = driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[1]/div/div[1]/a")
 
         park_instance.click()
-        self.assertEqual("http://parksr.us/parks/1", driver.current_url)
+        self.assertEqual("http://parksr.us/#/parks/1", driver.current_url)
 
         time.sleep(1)
 
@@ -125,17 +125,17 @@ class SeleniumTests(unittest.TestCase):
         time.sleep(1)
 
         driver.find_element_by_link_text('Parks').click()
-        self.assertEqual("http://parksr.us/#/parks", driver.current_url)
+        self.assertEqual("http://parksr.us/#/parks/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.find_element_by_link_text('Cities').click()
-        self.assertEqual("http://parksr.us/#/cities", driver.current_url)
+        self.assertEqual("http://parksr.us/#/cities/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.find_element_by_link_text('Snapshots').click()
-        self.assertEqual("http://parksr.us/#/snapshots", driver.current_url)
+        self.assertEqual("http://parksr.us/#/snapshots/pages/1", driver.current_url)
 
         time.sleep(1)
 
@@ -147,66 +147,66 @@ class SeleniumTests(unittest.TestCase):
         driver.get(self.site)
 
         driver.find_element_by_link_text('Parks').click()
-        self.assertEqual("http://parksr.us/#/parks", driver.current_url)
+        self.assertEqual("http://parksr.us/#/parks/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.back()
-        self.assertEqual("http://parksr.us/", driver.current_url)
+        self.assertEqual("http://parksr.us/#/", driver.current_url)
 
         time.sleep(1)
 
         driver.forward()
-        self.assertEqual("http://parksr.us/#/parks", driver.current_url)
+        self.assertEqual("http://parksr.us/#/parks/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.refresh()
-        self.assertEqual("http://parksr.us/#/parks", driver.current_url)
+        self.assertEqual("http://parksr.us/#/parks/pages/1", driver.current_url)
 
     def test_back_and_forward_cities(self):
         driver = self.driver
         driver.get(self.site)
 
         driver.find_element_by_link_text('Cities').click()
-        self.assertEqual("http://parksr.us/#/cities", driver.current_url)
+        self.assertEqual("http://parksr.us/#/cities/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.back()
-        self.assertEqual("http://parksr.us/", driver.current_url)
+        self.assertEqual("http://parksr.us/#/", driver.current_url)
 
         time.sleep(1)
 
         driver.forward()
-        self.assertEqual("http://parksr.us/#/cities", driver.current_url)
+        self.assertEqual("http://parksr.us/#/cities/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.refresh()
-        self.assertEqual("http://parksr.us/#/cities", driver.current_url)
+        self.assertEqual("http://parksr.us/#/cities/pages/1", driver.current_url)
 
     def test_back_and_forward_snapshots(self):
         driver = self.driver
         driver.get(self.site)
 
         driver.find_element_by_link_text('Snapshots').click()
-        self.assertEqual("http://parksr.us/#/snapshots", driver.current_url)
+        self.assertEqual("http://parksr.us/#/snapshots/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.back()
-        self.assertEqual("http://parksr.us/", driver.current_url)
+        self.assertEqual("http://parksr.us/#/", driver.current_url)
 
         time.sleep(1)
 
         driver.forward()
-        self.assertEqual("http://parksr.us/#/snapshots", driver.current_url)
+        self.assertEqual("http://parksr.us/#/snapshots/pages/1", driver.current_url)
 
         time.sleep(1)
 
         driver.refresh()
-        self.assertEqual("http://parksr.us/#/snapshots", driver.current_url)
+        self.assertEqual("http://parksr.us/#/snapshots/pages/1", driver.current_url)
 
     def test_back_and_forward_about(self):
         driver = self.driver
@@ -218,7 +218,7 @@ class SeleniumTests(unittest.TestCase):
         time.sleep(1)
 
         driver.back()
-        self.assertEqual("http://parksr.us/", driver.current_url)
+        self.assertEqual("http://parksr.us/#/", driver.current_url)
 
         time.sleep(1)
 
