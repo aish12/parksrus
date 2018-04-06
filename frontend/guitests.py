4b11e5b74dd163d88a79c2cb24c0984e12970cc6
Ben Chen <btchen@utexas.edu>
2018-04-05 19:00:54 -0500

    added tests for filtering/sorting/pagination

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -4,8 +4,8 @@
 Selenium tests
 """
 from selenium import webdriver
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 #from selenium.webdriver.support.ui import Select
 import unittest
 import time
@@ -230,6 +230,191 @@ class SeleniumTests(unittest.TestCase):
         driver.refresh()
         self.assertEqual("http://parksr.us/#/about", driver.current_url)
 
+
+    def test_pagination_parks(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Parks').click()
+
+        time.sleep(1)
+        
+        driver.find_element_by_link_text('2').click()
+        self.assertEqual("http://parksr.us/#/parks/pages/2", driver.current_url)
+
+        time.sleep(1)
+
+        driver.find_element_by_link_text('3').click()
+        self.assertEqual("http://parksr.us/#/parks/pages/3", driver.current_url)
+
+        time.sleep(1)
+
+        #driver.find_element_by_link_text('<').click()
+        #self.assertEqual("http://parksr.us/#/parks/pages/1", driver.current_url)
+
+    def test_pagination_cities(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Cities').click()
+
+        time.sleep(1)
+        
+        driver.find_element_by_xpath("//a[@href='#/cities/pages/2']").click()
+        self.assertEqual("http://parksr.us/#/cities/pages/2", driver.current_url)
+
+        time.sleep(1)
+
+        driver.find_element_by_xpath("//a[@href='#/cities/pages/3']").click()
+        self.assertEqual("http://parksr.us/#/cities/pages/3", driver.current_url)
+
+        time.sleep(1)
+
+        #driver.find_element_by_link_text('<').click()
+        #self.assertEqual("http://parksr.us/#/cities/pages/1", driver.current_url)
+
+    def test_pagination_snapshots(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Snapshots').click()
+
+        time.sleep(1)
+        
+        driver.find_element_by_link_text('2').click()
+        self.assertEqual("http://parksr.us/#/snapshots/pages/2", driver.current_url)
+
+        time.sleep(1)
+
+        driver.find_element_by_link_text('3').click()
+        self.assertEqual("http://parksr.us/#/snapshots/pages/3", driver.current_url)
+
+        time.sleep(1)
+
+        #driver.find_element_by_link_text('<').click()
+        #self.assertEqual("http://parksr.us/#/snapshots/pages/1", driver.current_url)
+
+    def test_filter_parks(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Parks').click()
+
+        time.sleep(1)
+
+        driver.find_element_by_class_name('Select-placeholder').click()
+
+        time.sleep(1)
+
+        filter_element = driver.find_element_by_xpath("//input[@aria-activedescendant='react-select-2--option-0']")
+        filter_element.send_keys('Arizona')
+        filter_element.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        park_instance = driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[1]/div/div[1]/a")
+        park_instance.click()
+
+        time.sleep(1)
+
+        self.assertEqual("http://parksr.us/#/parks/58", driver.current_url)
+
+    def test_filter_cities(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Cities').click()
+
+        time.sleep(1)
+
+        filter_element = driver.find_element_by_class_name('Select-placeholder')
+        filter_element.click()
+
+        time.sleep(1)
+
+        filter_element = driver.find_element_by_class_name("Select-input>input")
+        filter_element.send_keys('Arizona')
+        filter_element.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        city_instance = driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[1]/div/div[1]/a")
+        city_instance.click()
+
+        time.sleep(1)
+
+        self.assertEqual("http://parksr.us/#/cities/4", driver.current_url)
+
+    def test_sort_parks(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Parks').click()
+
+        time.sleep(1)
+
+        driver.find_element_by_xpath("(//div[@class='Select-placeholder'])[2]").click()
+
+        time.sleep(1)
+
+        sort_element = driver.find_element_by_xpath("(//input)[2]")
+        sort_element = sort_element
+        sort_element.send_keys('review_data')
+        sort_element.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        park_instance = driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[1]/div/div[1]/a")
+        park_instance.click()
+
+        time.sleep(1)
+
+        self.assertEqual("http://parksr.us/#/parks/197", driver.current_url)
+
+
+    def test_sort_cities(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Cities').click()
+
+        time.sleep(1)
+
+        driver.find_element_by_xpath("(//div[@class='Select-placeholder'])[2]").click()
+
+        time.sleep(1)
+
+        sort_element = driver.find_element_by_xpath("(//input)[2]")
+        sort_element = sort_element
+        sort_element.send_keys('num_parks')
+        sort_element.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        city_instance = driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[1]/div/div[1]/a")
+        city_instance.click()
+
+        time.sleep(1)
+
+        self.assertEqual("http://parksr.us/#/cities/18", driver.current_url)
+
+    def test_sort_snapshots(self):
+        driver = self.driver
+        driver.get(self.site)
+        driver.find_element_by_link_text('Snapshots').click()
+
+        time.sleep(1)
+
+        driver.find_element_by_xpath("//div[@class='Select-placeholder']").click()
+
+        time.sleep(1)
+
+        sort_element = driver.find_element_by_xpath("//input")
+        sort_element = sort_element
+        sort_element.send_keys('views')
+        sort_element.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        snapshot_instance = driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[1]/div/div[1]/a")
+        snapshot_instance.click()
+
+        time.sleep(1)
+
+        self.assertEqual("http://parksr.us/#/snapshots/652", driver.current_url)
 
     def tearDown(self):
         self.driver.close()
