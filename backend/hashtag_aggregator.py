fa934a5e6e141da8eebb30bff375f80d67ba5295
Chimdindu Orakwue <denalexorakwue1@gmail.com>
2018-04-09 16:37:31 -0500

    fixing tags

--- backend/hashtag_aggregator.py
+++ backend/hashtag_aggregator.py
@@ -14,11 +14,32 @@ def get_tags():
 
 	return tag_set
 
+def filter_tags():
+
+	snapshots = db.session.query(Snapshot).all()
+	for snapshot in snapshots:
+		tags = snapshot.tags.split(',')
+		new_tags = []
+		for tag in tags:
+			if not omit(tag):
+				new_tags.append(tag)
+		snapshot.set_tags(','.join(new_tags))
+		db.session.merge(snapshot)
+        db.session.commit()
+
+def omit():
+	omitted =frozenset("uploaded:by=instagram", "instagram", "foursquare", "square", "flickstagram")
+	for word in omitted:
+		if word in tag:
+			return True
+	return False
+
 def main():
 	tags = list(get_tags())
 	with open("tags.json", "w+") as outfile:
 		json.dump(tags, outfile)
 
 if __name__ == '__main__':
+	filter_tags()
 	main()
 
