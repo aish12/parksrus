05022c2364e1e285e6bbd510f2602fc20dd39bc3
Chimdindu Orakwue <denalexorakwue1@gmail.com>
2018-04-15 23:51:38 -0500

    indentation error fix

--- backend/main.py
+++ backend/main.py
@@ -40,32 +40,32 @@ with app.app_context():
 
 @app.route('api/unpaginated/parks')
 def unpaginated_parks():
-	parks_list = []
-	parks = db.session.query(Park).all()
-	for park in parks:
-		parks_dict = {}
-		parks_dict['id'] = park.id
-		parks_dict['name'] = park.name
-		parks_dict['website'] = park.website
-		parks_dict['city_id'] = park.city_id
-		parks_dict['address'] = park.address
-		parks_dict['state'] = park.state
-		parks_dict['country'] = park.country
-		parks_dict['rating'] = float(park.review_data)
-		parks_dict['phone_number'] = park.phone_number
-		parks_dict['latitude'] = float(park.latitude)
-		parks_dict['longitude'] = float(park.longitude)
-		parks_dict['image_uri'] = park.image_uri
-		parks_list.append(parks_dict)
-	return json.dumps(parks_list)
+    parks_list = []
+    parks = db.session.query(Park).all()
+    for park in parks:
+        parks_dict = {}
+        parks_dict['id'] = park.id
+        parks_dict['name'] = park.name
+        parks_dict['website'] = park.website
+        parks_dict['city_id'] = park.city_id
+        parks_dict['address'] = park.address
+        parks_dict['state'] = park.state
+        parks_dict['country'] = park.country
+        parks_dict['rating'] = float(park.review_data)
+        parks_dict['phone_number'] = park.phone_number
+        parks_dict['latitude'] = float(park.latitude)
+        parks_dict['longitude'] = float(park.longitude)
+        parks_dict['image_uri'] = park.image_uri
+        parks_list.append(parks_dict)
+    return json.dumps(parks_list)
 
 
 @app.route('api/unpaginated/cities')
 def unpaginated_cities():
-	cities_list = []
-	cities = db.session.query(City).all()
+    cities_list = []
+    cities = db.session.query(City).all()
     for city in cities:
-    	city_dict = {}
+        city_dict = {}
         city_dict['id'] = city.id
         city_dict['name'] = city.name
         city_dict['longitude'] = float(city.longitude)
@@ -80,21 +80,21 @@ def unpaginated_cities():
 
 @app.route('api/unpaginated/snapshots')
 def unpaginated_snapshots():
-	snapshots_list = []
-	snapshots = db.session.query(Snapshot).all()
-	for snapshot in snapshots:
-		snapshot_dict = {}
-		snapshot_dict['id'] = snapshot.id
-		snapshot_dict['views'] = snapshot.views
-		snapshot_dict['image_uri'] = snapshot.image_uri
-		snapshot_dict['park_id'] = snapshot.park_id
-		snapshot_dict['city_id'] = snapshot.city_id
-		snapshot_dict['tags'] = snapshot.tags
-		snapshot_dict['date'] = snapshot.date
-		snapshot_dict['longitude'] = float(snapshot.longitude)
-		snapshot_dict['latitude'] = float(snapshot.latitude)
-		snapshots_list.append(snapshot_dict)
-	return json.dumps(snapshots_list)
+    snapshots_list = []
+    snapshots = db.session.query(Snapshot).all()
+    for snapshot in snapshots:
+        snapshot_dict = {}
+        snapshot_dict['id'] = snapshot.id
+        snapshot_dict['views'] = snapshot.views
+        snapshot_dict['image_uri'] = snapshot.image_uri
+        snapshot_dict['park_id'] = snapshot.park_id
+        snapshot_dict['city_id'] = snapshot.city_id
+        snapshot_dict['tags'] = snapshot.tags
+        snapshot_dict['date'] = snapshot.date
+        snapshot_dict['longitude'] = float(snapshot.longitude)
+        snapshot_dict['latitude'] = float(snapshot.latitude)
+        snapshots_list.append(snapshot_dict)
+    return json.dumps(snapshots_list)
 
 # search method
 def search(model, query_val):
