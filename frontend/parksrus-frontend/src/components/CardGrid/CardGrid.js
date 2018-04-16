0c117dadcd82ec528a29c4df607934262063e3b5
Trenton Beckendorff <trentonbeckendorff@utexas.edu>
2018-04-16 12:16:38 -0500

    Added additional card information for parks, cities.

--- frontend/parksrus-frontend/src/components/CardGrid/CardGrid.js
+++ frontend/parksrus-frontend/src/components/CardGrid/CardGrid.js
@@ -20,11 +20,13 @@ class CardGrid extends Component {
             <img src={entity.image_uri} className="CardImage"/>
             <div className={["CardContent", "CardHover"].join(' ')} >
               {entity.name && <h1 className={["CardContent", "CardHeader"].join(' ')}>{entity.name}</h1>}
-              {entity.state && <p className={["CardContent", "CardSubtitle"].join(' ')}>{entity.state}</p>}
+              {entity.city && entity.city.name && <div><h4>{entity.city.name}</h4></div>}
+              {entity.state && <h5>{entity.state}</h5>}
               {entity.num_parks && <div><h3>{entity.num_parks}</h3><p>Total Parks</p></div>}
               {entity.views && <div><h3>{entity.views}</h3><p>Views</p></div>}
               {entity.tags && <div><h3>Tags</h3><p>{entity.tags.split(',').map(hashtag => <Badge>{"#" + hashtag}</Badge>)}</p></div>}
               {entity.review_data && <div><h3>{entity.review_data}</h3><p>Avg. Rating</p></div>}
+              {entity.description && <div><p>{entity.description.split(' ').slice(0, 50).join(' ')}</p></div>}
               {entity.park && entity.park.name && <div><h3>Location</h3><p>{entity.park.name}</p></div>}
             </div>
           </Link>
