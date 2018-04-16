41eaa6e8f0140b28c5d079e94a9e52ec1a374127
Trenton Beckendorff <trentonbeckendorff@utexas.edu>
2018-04-16 12:21:32 -0500

    Adding build.

--- frontend/parksrus-frontend/src/components/Pages/VisualizationPage/VisualizationPage.js
+++ frontend/parksrus-frontend/src/components/Pages/VisualizationPage/VisualizationPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
 import { } from 'react-bootstrap';
-import Page from '../../Page/Page'
-import axios from 'axios'
+import Page from '../../Page/Page';
+import axios from 'axios';
+
+let d3 = require('d3');
 
 class VisualizationPage extends React.Component {
   constructor(props) {
@@ -12,7 +14,8 @@ class VisualizationPage extends React.Component {
       isLoaded: false,
       coffeeShops: [],
       scenicLocations: [],
-      snapshots: []
+      snapshots: [],
+      cities: []
     }
   }
 
@@ -28,15 +31,21 @@ class VisualizationPage extends React.Component {
     return axios.get('http://api.espressoyoself.me/snapshots');
   }
 
+  getCities() {
+    return axios.get('http://api.espressoyoself.me/getcities');
+  }
+
+
   componentWillMount() {
     const scope = this;
-    axios.all([scope.getCoffeeShops(), scope.getScenicLocations(), scope.getSnapshots()])
-    .then(axios.spread(function(coffeeShops, scenicLocations, snapshots) {
+    axios.all([scope.getCoffeeShops(), scope.getScenicLocations(), scope.getSnapshots(), scope.getCities()])
+    .then(axios.spread(function(coffeeShops, scenicLocations, snapshots, cities) {
       scope.setState({
         isLoaded: true,
         coffeeShops: coffeeShops.data,
         scenicLocations: scenicLocations.data,
-        snapshots: snapshots.data
+        snapshots: snapshots.data,
+        cities: cities.data
       });
     })).catch(function(error) {
       console.error(error);
@@ -47,8 +56,47 @@ class VisualizationPage extends React.Component {
     });
   }
 
+  getNodes() {
+    let nodes = [];
+    this.state.coffeeShops.forEach(shop => {
+      nodes.push({id: shop.shop_id + "/" + shop.shop_name, group: 1, label: shop.shop_name, level: 2});
+    });
+    this.state.scenicLocations.forEach(location => {
+      nodes.push({id: location.scenic_id + "/" + location.scenic_name, group: 2, label: location.scenic_name, level: 2});
+    });
+    this.state.snapshots.forEach(snapshot => {
+      nodes.push({id: snapshot.snap_id + "/" + snapshot.snap_name, group: 3, label: snapshot.snap_name, level: 2});
+    });
+    this.state.cities.forEach(city => {
+      nodes.push({id: city.city_id, group: 0, label: city.city_name, level: 1});
+    });
+    nodes.push({id: 0, group: 0, label: "EspressoYoself.me", level: 0});
+  }
+
+  getEdges() {
+    let edges = [];
+    this.state.coffeeShops.forEach(shop => {
+      edges.push({target: shop.shop_id + "/" + shop.shop_name, source: shop.city_id, strength: 1});
+    });
+    this.state.scenicLocations.forEach(location => {
+      edges.push({target: location.scenic_id + "/" + location.scenic_name, source: location.city_id, strength: 1});
+    });
+    this.state.snapshots.forEach(snapshot => {
+      edges.push({target: snapshot.snap_id + "/" + snapshot.snap_name, source: snapshot.city_id, strength: 1});
+    });
+  }
+
   render() {
     console.log(this.state.coffeeShops, this.state.scenicLocations, this.state.snapshots);
+    const width = 1000;
+    const height = 1000;
+    let svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+
+
+
+
+    let nodes = this.getNodes();
+    let edges = this.getEdges();
     return <div>
       <Page>
 
