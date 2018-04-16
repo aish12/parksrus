5be1907841e76ecbe0cbe0d816f7f3170858c60f
aish12 <aishwarya_shashidhar@yahoo.com>
2018-04-16 11:43:05 -0500

    close #72

--- DummyTest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''
-Dummy test file
-for travis purposes
-'''
-
-def dummy_test(a, b):
-	return a + b
--- RunParksTests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-''' Test class
-'''
-# ----
-# main
-# ----
-
-if __name__ == "__main__":
-    assert 1 == 1
